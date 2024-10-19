a2cc1bb39e7ca2ce7a51f43afd128c490c527b1a
incredibotsquad <146580828+incredibotsquad@users.noreply.github.com>
2024-10-18 17:24:59 -0700

    optimizations and drive practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -8,7 +8,9 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class RobotHardware {
@@ -27,6 +29,7 @@ public class RobotHardware {
      private DcMotor odoFront;
      private Servo clawServo;
      private CRServo intakeServo;
+
 
      //making constructor
      public RobotHardware(HardwareMap hwMap) {
@@ -65,6 +68,7 @@ public class RobotHardware {
          rightArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
          rightSlideMotor = hwMap.get(DcMotorEx.class, "RSMotor");
+         rightSlideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
          rightSlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
          rightSlideMotor.setTargetPosition(0);
          rightSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -78,34 +82,43 @@ public class RobotHardware {
          intakeServo = hwMap.get(CRServo.class, "intakeServo");
 
          imu = hwMap.get(IMU.class, "imu");
+
      }
+
      //functions
-     public void setSlidePositionAndPower(int pos, double power) {
-         rightSlideMotor.setPower(power);
+    // Sets the slide's position and velocity
+     public void setSlidePositionAndVelocity(int pos, double velocity) {
          rightSlideMotor.setTargetPosition(pos);
+         rightSlideMotor.setVelocity(velocity);
          rightSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
      }
 
-    public void setLeftArmPositionAndPower(int pos, double power) {
+    // Sets the left arm's position and velocity
+    public void setLeftArmPositionAndVelocity(int pos, double velocity) {
         leftArmMotor.setTargetPosition(pos);
         //leftArmMotor.setPower(power);
-        leftArmMotor.setVelocity(2100);
+        leftArmMotor.setVelocity(velocity);
         leftArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
-    public void setRightArmPositionAndPower(int pos, double power) {
-        rightArmMotor.setPower(power);
+
+    //Sets the right arm's position and velocity
+    public void setRightArmPositionAndVelocity(int pos, double velocity) {
         rightArmMotor.setTargetPosition(pos);
+        rightArmMotor.setVelocity(velocity);
         rightArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
+    //returns the robots yaw as radians
     public double getRobotYawRadians() {
          return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
     }
 
+    //returns the robots yaw as degrees
     public double getRobotYawDegrees() {
         return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
     }
 
+    //sets the drive motor's powers
     public void setDriveMotorPowers(double rfPower, double lfPower, double rbPower, double lbPower) {
          rightFrontDriveMotor.setPower(rfPower);
          leftFrontDriveMotor.setPower(lfPower);
@@ -113,6 +126,7 @@ public class RobotHardware {
          leftBackDriveMotor.setPower(lbPower);
      }
 
+     //resets each individual odo wheel
      public void resetRightOdo() {
          odoRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
      }
@@ -125,50 +139,71 @@ public class RobotHardware {
         odoFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
      }
 
-
+    //resets all the odo wheels at the same time
      public void resetAllOdo() {
         resetRightOdo();
         resetLeftOdo();
         resetFrontOdo();
      }
 
-    public void operateClawServo(boolean open) {
-         clawServo.setPosition(open?1:0);
+     //decides either to open or close the claw servo
+    public void operateClawServo(boolean open, double openPosition, double closePosition) {
+         clawServo.setPosition(open? openPosition: closePosition);
     }
+
+    // Decides if the intake servo should spin clockwise or counter clockwise
     public void operateIntakeServo(boolean clockwiseSpin, boolean counterClockwiseSpin) {
         if (clockwiseSpin){
-            intakeServo.setPower(1);
+            if (intakeServo.getPower() > 0) {
+                intakeServo.setPower(0);
+            } else {
+                intakeServo.setPower(1);
+            }
         }
         else if (counterClockwiseSpin){
-            intakeServo.setPower(-1);
+            if (intakeServo.getPower() < 0) {
+                intakeServo.setPower(0);
+            } else {
+                intakeServo.setPower(-1);
+            }
         }
-        else{
+        else {
             intakeServo.setPower(0);
         }
     }
 
+    // Returns the right odo wheel value
     public double getRightOdoMM() {
          return getMMTraveled(odoRight.getCurrentPosition());
      }
 
+    // Returns the left odo wheel value
     public double getLeftOdoMM() {
         return getMMTraveled(odoLeft.getCurrentPosition());
     }
 
+    // returns the front odo wheel value
     public double getFrontOdoMM() {
         return getMMTraveled(odoFront.getCurrentPosition());
     }
 
+    // Returns the distance traveled in mm
     public double getMMTraveled(int ticks) {
          return RobotConstants.MMPerTick * ticks;
     }
 
+    // Returns the right arm motor position
     public int getRightArmMotorPos(){
          return rightArmMotor.getCurrentPosition();
     }
 
+    // Returns the left arm position
     public int getLeftArmMotorPos(){
         return leftArmMotor.getCurrentPosition();
+    }
+
+    public int getRightSlidePos(){
+        return rightSlideMotor.getCurrentPosition();
     }
 
 }
