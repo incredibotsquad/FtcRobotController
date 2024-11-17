44e2a4dbe0e045f2b0276dce10501ab628ee9a6b
incredibotsquad <146580828+incredibotsquad@users.noreply.github.com>
2024-11-16 21:35:26 -0800

    LM2 optimizations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ArmMotionAsRRAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ArmMotionAsRRAction.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.teamcode.RobotHardware;
 
 public class ArmMotionAsRRAction implements Action {
@@ -11,6 +15,7 @@ public class ArmMotionAsRRAction implements Action {
     private int armVelocity;
     private boolean initialized = false;
     private boolean waitForAction = false;
+    private ElapsedTime timer;
 
     public  ArmMotionAsRRAction(RobotHardware robotHardware, int armPosition, int armVelocity, boolean waitForAction) {
         this.myHardware = robotHardware;
@@ -24,11 +29,16 @@ public class ArmMotionAsRRAction implements Action {
     public boolean run (@NonNull TelemetryPacket packet) {
         if (!initialized) {
             myHardware.setClawArmPositionAndVelocity(armPosition, armVelocity);
+            timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
             initialized = true;
         }
 
         if (waitForAction) {
-            return myHardware.isClawArmMotorBusy();
+            Log.i("=== INCREDIBOTS ===", "|+|+|+|+|+|+|+|+|+|+|+|+|+|+: ");
+            Log.i("=== INCREDIBOTS ===", "WAITING FOR ARM. MOTOR BUSY: " + myHardware.isClawArmMotorBusy());
+            Log.i("=== INCREDIBOTS ===", "WAITING FOR ARM. TIMER: " + timer.milliseconds());
+            Log.i("=== INCREDIBOTS ===", "|+|+|+|+|+|+|+|+|+|+|+|+|+|+: ");
+            return (myHardware.isClawArmMotorBusy() && (timer.milliseconds() < 700));
         }
 
         return false;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ClawMotionAsRRAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ClawMotionAsRRAction.java
@@ -16,15 +16,17 @@ public class ClawMotionAsRRAction implements Action {
     private double closePosition;
     private boolean initialized = false;
     private boolean waitForAction = false;
+    private boolean shortWait = false;
     private ElapsedTime timer;
 
-    public ClawMotionAsRRAction(RobotHardware robotHardware, boolean open, double openPosition, double closePosition, boolean waitForAction) {
+    public ClawMotionAsRRAction(RobotHardware robotHardware, boolean open, double openPosition, double closePosition, boolean waitForAction, boolean shortWait) {
         this.myHardware = robotHardware;
         this.open = open;
         this.openPosition = openPosition;
         this.closePosition = closePosition;
         this.initialized = false;
         this.waitForAction = waitForAction;
+        this.shortWait = shortWait;
     }
 
     @Override
@@ -36,7 +38,11 @@ public class ClawMotionAsRRAction implements Action {
         }
 
         if (waitForAction) {
-            boolean timerPending = timer.milliseconds() < 500;
+            int timeDuration = 400;
+            if (shortWait) {
+                timeDuration = 200;
+            }
+            boolean timerPending = timer.milliseconds() < timeDuration;
             Log.i("=== INCREDIBOTS ===", "INSIDE CLAWMOTIONASRRACTION - WAITING FOR CLAW: " + timerPending);
             //tell RR we need to keep running if 500 ms has not elapsed
             return (timerPending);
