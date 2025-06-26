55d0d48a5c7df1566a4d8b89b07827509e6c847d
incredibotsquad <146580828+incredibotsquad@users.noreply.github.com>
2025-06-25 22:05:17 -0700

    Revert "shoulder servo dampening "
    This reverts part of commit bc623cf292cd7ac62282b4345faef0c0e09f4173.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/auto/actions/VerticalShoulderAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/auto/actions/VerticalShoulderAction.java
@@ -1,9 +1,5 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.auto.actions;
 
-import static com.qualcomm.robotcore.util.ElapsedTime.Resolution.MILLISECONDS;
-
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
@@ -15,68 +11,41 @@ import org.firstinspires.ftc.teamcode.RobotHardware;
 
 public class VerticalShoulderAction implements Action {
     private RobotHardware robotHardware;
-    private double targetPosition;
+    private double position;
     private boolean initialized = false;
     private boolean waitForAction = false;
     private boolean shortWait = false;
     private ElapsedTime timer;
 
-    private ElapsedTime incrementalTimer;
-
     public VerticalShoulderAction(RobotHardware robotHardware, double position, boolean waitForAction, boolean shortWait) {
         this.robotHardware = robotHardware;
-        this.targetPosition = position;
+        this.position = position;
+        this.initialized = false;
         this.waitForAction = waitForAction;
         this.shortWait = shortWait;
     }
 
     @Override
     public boolean run (@NonNull TelemetryPacket packet) {
-        double currentServoPosition = robotHardware.getVerticalShoulderServoPosition();
-        double delta = targetPosition - currentServoPosition;
-
-
-        if (currentServoPosition <= (RobotConstants.VERTICAL_SHOULDER_TRANSFER - 0.1) &&
-                targetPosition > RobotConstants.VERTICAL_SHOULDER_HANG_SPECIMEN &&
-                targetPosition <= RobotConstants.VERTICAL_SHOULDER_TRANSFER &&
-                delta > 0.1) {
-
-            if (incrementalTimer == null || incrementalTimer.milliseconds() > 50) {
-                if (incrementalTimer == null) {
-                    incrementalTimer = new ElapsedTime(MILLISECONDS);
-                }
-
-                Log.i("=== INCREDIBOTS / VerticalShoulderAction ===", " Incrementally setting servo position. Current: " + currentServoPosition + " Target: " + targetPosition);
-
-                robotHardware.setVerticalShoulderServoPosition(currentServoPosition + delta / 2);
-                incrementalTimer.reset();
-            }
-
-            return true;
+        if (!initialized) {
+            timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+            robotHardware.setVerticalShoulderServoPosition(position);
+            initialized = true;
         }
-        else {
-            if (!initialized) {
-                Log.i("=== INCREDIBOTS / VerticalShoulderAction ===", " COMPLETED INCREMENTAL SERVO OPERATION - STRAIGHT SET CALL");
 
-                initialized = true;
-                robotHardware.setVerticalShoulderServoPosition(targetPosition);
-                timer = new ElapsedTime(MILLISECONDS);
+        if (waitForAction) {
+            if (shortWait) {
+                //return immediately if shoulder is already at position
+                if (Math.abs(robotHardware.getVerticalShoulderServoPosition() - position) < RobotConstants.VERTICAL_SHOULDER_POSITION_TOLERANCE)
+                    return false;
+
+                return (timer.milliseconds() < 250);
             }
 
-            if (waitForAction) {
-                if (shortWait) {
-                    //return immediately if shoulder is already at position
-                    if (Math.abs(robotHardware.getVerticalShoulderServoPosition() - targetPosition) < RobotConstants.VERTICAL_SHOULDER_POSITION_TOLERANCE)
-                        return false;
-
-                    return (timer.milliseconds() < 250);
-                }
-
-                //tell RR we need to keep running if shoulder is not in the right position yet
-                return (Math.abs(robotHardware.getVerticalShoulderServoPosition() - targetPosition) > RobotConstants.VERTICAL_SHOULDER_POSITION_TOLERANCE);
-            }
-
-            return false;
+            //tell RR we need to keep running if shoulder is not in the right position yet
+            return (Math.abs(robotHardware.getVerticalShoulderServoPosition() - position) > RobotConstants.VERTICAL_SHOULDER_POSITION_TOLERANCE);
         }
+
+        return false;
     }
 }
