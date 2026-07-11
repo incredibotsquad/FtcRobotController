d2e857925e8f36b900945e021d83127977ddff26
Varun Saini <varun@varunsaini.com>
2026-07-11 13:16:59 -0700

    Add optional moving shot compensation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OdometrySubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OdometrySubsystem.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -19,6 +20,10 @@ public class OdometrySubsystem extends SubsystemBase {
 
     private final GoBildaPinpointDriver pinpoint;
     private Pose2d currentPose = new Pose2d();
+    private Pose2d previousPose = new Pose2d();
+    private Translation2d fieldVelocity = new Translation2d(0, 0);
+    private Translation2d fieldAcceleration = new Translation2d(0, 0);
+    private long previousUpdateNanos = 0;
 
     private TelemetryManager telemetry;
 
@@ -69,6 +74,18 @@ public class OdometrySubsystem extends SubsystemBase {
         return currentPose;
     }
 
+    public Translation2d getFieldVelocity() {
+        return fieldVelocity;
+    }
+
+    public Translation2d getFieldAcceleration() {
+        return fieldAcceleration;
+    }
+
+    public double getFieldSpeedInchesPerSecond() {
+        return Math.hypot(fieldVelocity.getX(), fieldVelocity.getY());
+    }
+
     // Allows you to reset the location (e.g., at the start of Autonomous)
     public void resetPose(double xInches, double yInches, double rotationDegrees) {
 
@@ -80,6 +97,12 @@ public class OdometrySubsystem extends SubsystemBase {
 
         //force an update so the hardware registers it
         pinpoint.update();
+
+        currentPose = new Pose2d(xInches, yInches, new Rotation2d(Math.toRadians(rotationDegrees)));
+        previousPose = currentPose;
+        fieldVelocity = new Translation2d(0, 0);
+        fieldAcceleration = new Translation2d(0, 0);
+        previousUpdateNanos = System.nanoTime();
     }
 
     @Override
@@ -93,7 +116,10 @@ public class OdometrySubsystem extends SubsystemBase {
         double yInches = pinpoint.getPosY(DistanceUnit.INCH);
         double headingRadians = pinpoint.getHeading(AngleUnit.RADIANS);
 
-        currentPose = new Pose2d(xInches, yInches, new Rotation2d(headingRadians));
+        Pose2d newPose = new Pose2d(xInches, yInches, new Rotation2d(headingRadians));
+        updateVelocityEstimate(newPose);
+
+        currentPose = newPose;
         CrossOpModeStorage.currentPose = currentPose;
 
 //        Log.i("Odometry", "X Position: " + xInches + " Y Position: " + yInches + " Heading: " + headingDegrees);
@@ -101,5 +127,31 @@ public class OdometrySubsystem extends SubsystemBase {
         telemetry.addData("Odometry: X Position", xInches);
         telemetry.addData("Odometry: Y Position", yInches);
         telemetry.addData("Odometry: Heading (Deg)", Math.toDegrees(headingRadians) + 360); //adding for ease of readability
+        telemetry.addData("Odometry: X Velocity", fieldVelocity.getX());
+        telemetry.addData("Odometry: Y Velocity", fieldVelocity.getY());
+        telemetry.addData("Odometry: Speed", getFieldSpeedInchesPerSecond());
+    }
+
+    private void updateVelocityEstimate(Pose2d newPose) {
+        long now = System.nanoTime();
+
+        if (previousUpdateNanos != 0) {
+            double dtSeconds = (now - previousUpdateNanos) / 1.0e9;
+
+            if (dtSeconds > 0.0) {
+                Translation2d previousVelocity = fieldVelocity;
+                fieldVelocity = new Translation2d(
+                        (newPose.getX() - previousPose.getX()) / dtSeconds,
+                        (newPose.getY() - previousPose.getY()) / dtSeconds
+                );
+                fieldAcceleration = new Translation2d(
+                        (fieldVelocity.getX() - previousVelocity.getX()) / dtSeconds,
+                        (fieldVelocity.getY() - previousVelocity.getY()) / dtSeconds
+                );
+            }
+        }
+
+        previousPose = newPose;
+        previousUpdateNanos = now;
     }
 }
